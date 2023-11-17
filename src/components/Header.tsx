cfb4ea50f7416537c0f981e7050a53c7696e66ca
CaiShengDa <godezero@live.com>
2023-11-17 22:56:19 +0800

    删除vconsole调试

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,11 @@
 import '../assetcs/css/header.css'
-import { Show,onMount } from 'solid-js'
+import { Show } from 'solid-js'
 import browser from '../utils/browser'
 
 interface Props {
   title: string
 }
 export default ({ title }: Props) => {
-  onMount(()=>{
-    setTimeout(()=>{
-      new window.VConsole()
-      console.log(browser)
-      console.log(window.navigator.userAgent)
-      },3000)
-  })
-  const handleClose = () => {
-    window.location.href = '/'
-  }
   return (
     <Show when={!browser.versions.weixin}>
           <header>
